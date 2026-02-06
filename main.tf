70d7aadea8f8e3dd81e67ac937b0576a5d363a15
KKHARA1 <52095270+KKHARA1@users.noreply.github.com>
2026-02-06 19:32:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,28 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example"
+terraform {
+backend "azurerm" {
+    storage_account_name = "kkstgnewdev"                                 
+    container_name       = "tfstate"                                  
+    key                  = "prod.terraform.tfstate"                   
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.43.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id = "b4461466-1e6b-4be2-bb70-1e96a72a41c8"
+
+}
+
+resource "azurerm_resource_group" "ngn" {
+  name     = "newkkrg"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "ngw" {
+  name     = "newkkrg1"
   location = "West Europe"
 }
